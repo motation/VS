e9c83511d2e3d96742d98203991c5d9d4e93f094
motation <sven-ole.fedders@haw-hamburg.de>
2015-10-31 16:36:58 +0100

    updated final project structure

--- monopoly/monopoly-service/src/main/java/de/hawhamburg/service/MonopolyServiceApplication.java
+++ monopoly/monopoly-service/src/main/java/de/hawhamburg/service/MonopolyServiceApplication.java
@@ -1,11 +1,11 @@
-package de.hawhamburg.service.dice;
+package de.hawhamburg.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiceServiceApplication {
+public class MonopolyServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DiceServiceApplication.class,args);
+        SpringApplication.run(MonopolyServiceApplication.class,args);
     }
 }
