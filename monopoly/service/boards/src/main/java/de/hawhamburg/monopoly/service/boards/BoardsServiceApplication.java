9a52c39eebf3d5609aab6382c9cde9a729c26f86
unknown <motation@github.com>
2015-11-15 13:57:07 +0100

    boards added

---
+++ monopoly/service/boards/src/main/java/de/hawhamburg/monopoly/service/boards/BoardsServiceApplication.java
@@ -0,0 +1,11 @@
+package de.hawhamburg.monopoly.service.boards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardsServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BoardsServiceApplication.class,args);
+    }
+}
---
+++ monopoly/service/boards/src/main/java/de/hawhamburg/monopoly/service/boards/model/Board.java
@@ -0,0 +1,7 @@
+package de.hawhamburg.monopoly.service.boards.model;
+
+/**
+ * Created by Ole on 15.11.2015.
+ */
+public class Board {
+}
