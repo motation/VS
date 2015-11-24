c7197ba6ba2d8478f764b4fce0a2bbc8923d5d59
jenz <jenz@silpion.de>
2015-11-24 16:46:20 +0100

    created Exceptions

---
+++ monopoly/service/boards/src/main/java/de/hawhamburg/monopoly/service/boards/exception/PlayerNotReadyException.java
@@ -0,0 +1,9 @@
+package de.hawhamburg.monopoly.service.boards.exception;
+
+/**
+ * Created by Marcus Jenz on 24.11.15.
+ *
+ * @author Marcus Jenz
+ */
+public class PlayerNotReadyException extends Exception {
+}
---
+++ monopoly/service/dice/src/main/java/de/hawhamburg/monopoly/service/dice/exception/InvalidRollException.java
@@ -0,0 +1,10 @@
+package de.hawhamburg.monopoly.service.dice.exception;
+
+/**
+ * Created by Marcus Jenz on 24.11.15.
+ *
+ * @author Marcus Jenz
+ */
+public class InvalidRollException extends Exception{
+
+}
