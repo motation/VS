f284d73d60b060a552713a4e65bec6e7615e8c47
unknown <motation@github.com>
2015-11-25 12:27:50 +0100

    client added

---
+++ monopoly/client/src/main/java/de/hawhamburg/monopoly/client/Client.java
@@ -0,0 +1,20 @@
+package de.hawhamburg.monopoly.client;
+
+/**
+ * Created by Ole on 25.11.2015.
+ */
+public class Client {
+
+    public void createGame(){
+
+    }
+
+    public void joinGame(){
+
+    }
+
+    public void roll(){
+
+    }
+
+}
---
+++ monopoly/client/src/main/java/de/hawhamburg/monopoly/client/ClientApplication.java
@@ -0,0 +1,11 @@
+package de.hawhamburg.monopoly.client;
+
+/**
+ * Created by Ole on 25.11.2015.
+ */
+public class ClientApplication {
+    public static void main(String[] args) {
+        Client client = new Client();
+
+    }
+}
