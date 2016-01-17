a2c14913320dfa29a09bc2145f35ecbeec09e7d5
Sven-Ole Fedders <motation@github.com>
2016-01-17 17:55:11 +0100

    service get url by name

--- monopoly/service/games/src/main/java/de/hawhamburg/monopoly/service/games/GamesServiceApplication.java
+++ monopoly/service/games/src/main/java/de/hawhamburg/monopoly/service/games/GamesServiceApplication.java
@@ -1,11 +1,39 @@
 package de.hawhamburg.monopoly.service.games;
 
+import de.hawhamburg.services.service.ServicesService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
+@ComponentScan({"de.hawhamburg.monopoly.service.games", "de.hawhamburg.services"})
 @SpringBootApplication
 public class GamesServiceApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GamesServiceApplication.class);
+
+    @Autowired
+    private ServicesService servicesService;
+
+    @PostConstruct
+    public void registerToDiscoveryService() {
+        de.hawhamburg.services.entity.Service service =
+                new de.hawhamburg.services.entity.Service("desrib", "gamesService", "games", "uri");
+
+        String result = servicesService.registerService(service);
+        if (result == null) {
+            LOG.warn("registration not successful!");
+        } else {
+            LOG.info("registration successful!");
+        }
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(GamesServiceApplication.class,args);
+        SpringApplication.run(GamesServiceApplication.class, args);
     }
 }
---
+++ monopoly/service/services/src/main/java/de/hawhamburg/services/entity/Services.java
@@ -0,0 +1,14 @@
+package de.hawhamburg.services.entity;
+
+import java.util.List;
+
+/**
+ * Created by Ole on 17.01.2016.
+ */
+public class Services {
+    private List<String> services;
+
+    public List<String> getServices() {
+        return services;
+    }
+}
