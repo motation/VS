57f2accba18cf0807fdf749e8447f520b31e7620
Sven-Ole Fedders <motation@github.com>
2016-01-17 20:36:18 +0100

    heartbeat controller added

---
+++ monopoly/service/services/src/main/java/de/hawhamburg/services/controller/ServiceController.java
@@ -0,0 +1,20 @@
+package de.hawhamburg.services.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Ole on 17.01.2016.
+ */
+@RestController
+public class ServiceController {
+    private static final Logger LOG = LoggerFactory.getLogger(ServiceController.class);
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public void serviceHeartbeat(){
+        LOG.info("heartbeat request");
+    }
+}
