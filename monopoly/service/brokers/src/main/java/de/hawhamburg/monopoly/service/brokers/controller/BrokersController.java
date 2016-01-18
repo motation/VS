90bfdd39c18f02b8b959a2adda948c3bb799fb85
Sven-Ole Fedders <motation@github.com>
2016-01-18 15:25:56 +0100

    broker stuff

--- monopoly/service/brokers/src/main/java/de/hawhamburg/monopoly/service/brokers/controller/BrokersController.java
+++ monopoly/service/brokers/src/main/java/de/hawhamburg/monopoly/service/brokers/controller/BrokersController.java
@@ -2,6 +2,9 @@ package de.hawhamburg.monopoly.service.brokers.controller;
 
 import de.hawhamburg.monopoly.service.brokers.service.BrokersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.ws.rs.Consumes;
@@ -19,5 +22,24 @@ public class BrokersController {
     @Autowired
     private BrokersService brokersService;
 
+    @RequestMapping(value = "/brokers/{gameid}", method = RequestMethod.PUT)
+    public void createBrokerForGame(@PathVariable final String gameid){
+
+    }
+
+    @RequestMapping(value="/brokers/{gameid}/places/{placeid}",method = RequestMethod.PUT)
+    public void registerAvailableProperties(@PathVariable final String gameid, @PathVariable final int placeid){
+
+    }
+
+    @RequestMapping(value="/brokers/{gameid}/places/{placeid}/visit/{playerid}",method = RequestMethod.POST)
+    public void registerPlayer(@PathVariable final String gameid, @PathVariable final int placeid,
+                               @PathVariable final String playerid){
+
+    }
+    @RequestMapping(value="/brokers/{gameid}/places/{placeid}/owner",method = RequestMethod.POST)
+    public void buyProperty(@PathVariable final String gameid, @PathVariable final int placeid){
+
+    }
 
 }
--- monopoly/service/brokers/src/main/java/de/hawhamburg/monopoly/service/brokers/service/BrokersService.java
+++ monopoly/service/brokers/src/main/java/de/hawhamburg/monopoly/service/brokers/service/BrokersService.java
@@ -1,11 +1,33 @@
 package de.hawhamburg.monopoly.service.brokers.service;
 
+import de.hawhamburg.monopoly.service.brokers.model.Broker;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Ole on 04.11.2015.
  */
 @Service
 public class BrokersService {
+    private Map<String, Broker> brokers;
+
+    @PostConstruct
+    private void init() {
+        brokers = new HashMap<>();
+    }
+
+    public boolean addBroker(String gameid) {
+        return (brokers.putIfAbsent(gameid, new Broker()) != null);
+    }
+
+    public Broker getBroker(String gameid){
+        return brokers.get(gameid);
+    }
+
+
+
 
 }
