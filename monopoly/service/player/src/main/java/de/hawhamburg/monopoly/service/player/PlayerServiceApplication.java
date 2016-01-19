1a42085eeabac8d6c8545a7bf0e7021f72cfa444
Sven-Ole Fedders <motation@github.com>
2016-01-19 21:48:30 +0100

    player fix

--- monopoly/service/player/src/main/java/de/hawhamburg/monopoly/service/player/PlayerServiceApplication.java
+++ monopoly/service/player/src/main/java/de/hawhamburg/monopoly/service/player/PlayerServiceApplication.java
@@ -7,9 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import javax.annotation.PostConstruct;
 
+@ComponentScan({"de.hawhamburg.monopoly.service.player", "de.hawhamburg.services"})
 @SpringBootApplication
 public class PlayerServiceApplication {
 
