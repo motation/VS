1283ed5bd1e4e3a06231782818a16a3170ca35af
Sven-Ole Fedders <motation@github.com>
2016-01-19 22:40:00 +0100

    getting ip adress

--- monopoly/service/games/src/main/java/de/hawhamburg/monopoly/service/games/GamesServiceApplication.java
+++ monopoly/service/games/src/main/java/de/hawhamburg/monopoly/service/games/GamesServiceApplication.java
@@ -5,18 +5,24 @@ import de.hawhamburg.services.service.ServicesService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @ComponentScan({"de.hawhamburg.monopoly.service.games", "de.hawhamburg.services"})
 @SpringBootApplication
 public class GamesServiceApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(GamesServiceApplication.class);
+
+    @Value("${server.port}")
+    private int port;
 
     @Autowired
     private ServicesService servicesService;
@@ -29,6 +35,18 @@ public class GamesServiceApplication {
         de.hawhamburg.services.entity.Service service =
                 new de.hawhamburg.services.entity.Service("This is a game service by LO", ServiceNames.NAME_OF_GAMES_SERVICE,
                         "Games Service Deluxe by LO", "https://vs-docker.informatik.haw-hamburg.de/ports/16310/games");
+
+        InetAddress ip;
+        try {
+            ip = InetAddress.getLocalHost();
+            LOG.info("Current IP address : " + ip.getHostAddress());
+            String urlOfService = ip.getHostAddress() + ":" + port;
+            service.setUri("http://"+urlOfService + "/games");
+        } catch (UnknownHostException e) {
+            LOG.info("error getting ip adress!");
+            e.printStackTrace();
+        }
+
 
         String result = servicesService.registerService(service);
         if (result == null) {
