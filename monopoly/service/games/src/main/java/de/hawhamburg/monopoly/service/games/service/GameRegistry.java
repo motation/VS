61612bfa7ca11ec9cbfd910e58f22a5a8dc312e8
jenz <jenz@silpion.de>
2016-01-20 09:47:19 +0100

    added uri to Gamecreation and added logging

--- monopoly/service/games/src/main/java/de/hawhamburg/monopoly/service/games/service/GameRegistry.java
+++ monopoly/service/games/src/main/java/de/hawhamburg/monopoly/service/games/service/GameRegistry.java
@@ -2,6 +2,9 @@ package de.hawhamburg.monopoly.service.games.service;
 
 import de.hawhamburg.monopoly.service.games.model.Game;
 import de.hawhamburg.monopoly.util.Components;
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -16,18 +19,30 @@ import java.util.UUID;
 public class GameRegistry {
     private List<Game> games;
 
+    private static final Logger LOG = LoggerFactory.getLogger(GameRegistry.class);
+
+
     @PostConstruct
     public void init() {
         games = new ArrayList<>();
     }
 
     public Game addGame(Components components) {
+
+        String id = System.currentTimeMillis()+"";
+        LOG.info("Trying to create game with id" + id);
         Game game = Game.builder()
-                .withGameid(System.currentTimeMillis()+"")
-                .withUri("")
+                .withGameid(id)
+                .withUri(components.getGame()+"/"+id)
                 .withComponents(components)
                 .build();
-        if(!games.add(game)) game = null;
+        if(!games.add(game)) {
+            game = null;
+            LOG.warn("Could not create game with id "+ id);
+        }else{
+            LOG.info("Created game with id" + id);
+        }
+
         return game;
     }
 
